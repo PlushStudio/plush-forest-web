a0de53531d08fca0b3e8d0c858aeeea85ae660ff
awicone <awiconet@gmail.com>
2021-10-29 15:15:09 +0300

    [PF-110] add missing option in accordion

--- src/components/App/shared-components/Accordion/Accordion.tsx
+++ src/components/App/shared-components/Accordion/Accordion.tsx
@@ -1,5 +1,5 @@
-import React, {useState} from 'react'
-import {Accordion, Card} from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Accordion, Card } from 'react-bootstrap'
 import s from './Accordion.module.scss'
 import '@/assets/styles/custom-elements/data-display/Accordion.scss'
 import accordionMinus from '@/assets/images/16-px-1-outlined-minus.svg'
@@ -10,68 +10,79 @@ import abstractIconHeart from '@/assets/images/abstract-icon-3.svg'
 
 
 export const LearnMoreAccordion = () => {
-    const [isOpenTabId, setIsOpenTabId] = useState(0)
+  const [isOpenTabId, setIsOpenTabId] = useState(0)
 
-    const handleTabClick = (tabId: number) => {
-        isOpenTabId === tabId ? setIsOpenTabId(-1) : setIsOpenTabId(tabId)
-    }
+  const handleTabClick = (tabId: number) => {
+    isOpenTabId === tabId ? setIsOpenTabId(-1) : setIsOpenTabId(tabId)
+  }
 
-    return (
-      <Accordion defaultActiveKey='0'>
-          <Card className={s.card}>
-              <Accordion.Toggle
-                className={`unselectable accordionText ${isOpenTabId === 0 ? s.opened : ''}`}
-                onClick={() => handleTabClick(0)} as={Card.Header} eventKey='0'>
-
-                  How it works
-                  {isOpenTabId === 0 && <img alt='show more' src={accordionMinus}/>}
-                  {isOpenTabId !== 0 && <img alt='show more' src={accordionPlus}/>}
-              </Accordion.Toggle>
-              <Accordion.Collapse eventKey='0'>
-                  <Card.Body>
-                      <div className={s.tabMainBlock}>
-                          To bring this forest to Plush family, we have partnered with Ecomatcher - a
-                          blockchain
-                          organization that has developed a unique
-                          technology to facilitate transparent tree planting between NGOs who plant and
-                          maintain
-                          the trees, and the people who want to support this initiative.
-                          By working with Ecomatcher we are able to provide precise tree tracking and
-                          ownership
-                          verification with blockchain technology.
-                      </div>
-                      <div className={s.tabAdditionalBlockContainer}>
-                          <div className={s.tabAdditionalBlock}>
-                              <div className={s.additionalLeftPull}>
-                                  <img src={abstractIconLeaf} alt={'leaf icon'}/>
-                              </div>
-                              <div className={`${s.additionalRightPull}  ${s.correctionStyle}`}>
-                                  NGO's from around the world plant the trees and capture tree data with
-                                  EcoMatcher's Treecoder blockchain technology.
-                              </div>
-                          </div>
-                          <div className={s.tabAdditionalBlock}>
-                              <div className={s.additionalLeftPull}>
-                                  <img src={abstractIconEye} alt={'eye icon'}/>
-                              </div>
-                              <div className={s.additionalRightPull}>
-                                  EcoMatcher inspects and validates information captured by NGO's, and
-                                  offers Plush to purchase any available forests.
-                              </div>
-                          </div>
-                          <div className={s.tabAdditionalBlock}>
-                              <div className={s.additionalLeftPull}>
-                                  <img src={abstractIconHeart} alt={'heart icon'}/>
-                              </div>
-                              <div className={s.additionalRightPull}>
-                                  Plush acquires a forest and further extends EcoMatcher’s technology to
-                                  assign a tree selected by parents to every child's unique Plush token.
-                              </div>
-                          </div>
-                      </div>
-                  </Card.Body>
-              </Accordion.Collapse>
-          </Card>
-      </Accordion>
-    )
+  return (
+    <Accordion defaultActiveKey='0'>
+      <Card className={s.card}>
+        <Accordion.Toggle
+          className={`unselectable accordionText ${isOpenTabId === 0 ? s.opened : ''}`}
+          onClick={() => handleTabClick(0)} as={Card.Header} eventKey='0'>
+          How it works
+          {isOpenTabId === 0 && <img alt='show more' src={accordionMinus} />}
+          {isOpenTabId !== 0 && <img alt='show more' src={accordionPlus} />}
+        </Accordion.Toggle>
+        <Accordion.Collapse eventKey='0'>
+          <Card.Body>
+            <div className={s.tabMainBlock}>
+              To bring this forest to Plush family, we have partnered with Ecomatcher - a
+              blockchain
+              organization that has developed a unique
+              technology to facilitate transparent tree planting between NGOs who plant and
+              maintain
+              the trees, and the people who want to support this initiative.
+              By working with Ecomatcher we are able to provide precise tree tracking and
+              ownership
+              verification with blockchain technology.
+            </div>
+            <div className={s.tabAdditionalBlockContainer}>
+              <div className={s.tabAdditionalBlock}>
+                <div className={s.additionalLeftPull}>
+                  <img src={abstractIconLeaf} alt={'leaf icon'} />
+                </div>
+                <div className={`${s.additionalRightPull}  ${s.correctionStyle}`}>
+                  NGO's from around the world plant the trees and capture tree data with
+                  EcoMatcher's Treecoder blockchain technology.
+                </div>
+              </div>
+              <div className={s.tabAdditionalBlock}>
+                <div className={s.additionalLeftPull}>
+                  <img src={abstractIconEye} alt={'eye icon'} />
+                </div>
+                <div className={s.additionalRightPull}>
+                  EcoMatcher inspects and validates information captured by NGO's, and
+                  offers Plush to purchase any available forests.
+                </div>
+              </div>
+              <div className={s.tabAdditionalBlock}>
+                <div className={s.additionalLeftPull}>
+                  <img src={abstractIconHeart} alt={'heart icon'} />
+                </div>
+                <div className={s.additionalRightPull}>
+                  Plush acquires a forest and further extends EcoMatcher’s technology to
+                  assign a tree selected by parents to every child's unique Plush token.
+                </div>
+              </div>
+            </div>
+          </Card.Body>
+        </Accordion.Collapse>
+      </Card>
+      <Card className={s.card}>
+        <Accordion.Toggle
+          className={`unselectable accordionText ${isOpenTabId === 3 && s.opened}`}
+          onClick={() => handleTabClick(3)} as={Card.Header} eventKey='3'>
+          About Plant Your Future NGO
+          {isOpenTabId === 3 && <img alt='show more' src={accordionMinus} />}
+          {isOpenTabId !== 3 && <img alt='show more' src={accordionPlus} />}
+        </Accordion.Toggle>
+        <Accordion.Collapse eventKey='3'>
+          <Card.Body>Hello! I'm another body</Card.Body>
+        </Accordion.Collapse>
+      </Card>
+    </Accordion>
+  )
 }
