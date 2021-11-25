b744a9658758055c9afe4c42621310b8bf5d2fe9
sasha_a <tastytea.devteam@gmail.com>
2021-11-25 21:19:04 +0700

    [PLF-536] Completed

--- src/config.d.ts
+++ src/config.d.ts
@@ -1,5 +1,6 @@
 interface Window {
-  config: Config
+  config: Config,
+  _paq: any,
 }
 
 interface Config {
@@ -7,5 +8,7 @@ interface Config {
   PLAI_CONTRACT_ADDRESS?: string
   CORE_CONTRACT_ADDRESS?: string
   CORE_GET_TREE_CONTRACT_ADDRESS?: string
-  NETWORK_ID?: string
+  NETWORK_ID?: string,
+  MATOMO_URL?: string,
+  MATOMO_SITE_ID?: string
 }
