aa67422543323dea49daaee41419e011b6b81cfe
awicone <awiconet@gmail.com>
2022-05-19 21:33:04 +0300

    [PF-198] - adaptive tree page, remove unused files and variables

--- src/components/Timeline/MessageInfo.tsx
+++ src/components/Timeline/MessageInfo.tsx
@@ -3,14 +3,12 @@ import s from './MessageInfo.module.scss'
 
 const MessageInfo: React.FC = () => {
   return (
-    <div>
-      <div className={s.messageInfo}>
-        <p>Your tree is managed by <span>Plant Your Future</span> - a sustainable agroforestry organization
-          dedicated
-          to restoring the Amazon by helping rainforest communities to plant native trees and improve
-          their livelihoods.</p>
-        <span>We are all connected</span>
-      </div>
+    <div className={s.messageInfo}>
+      <p>Your tree is managed by <span>Plant Your Future</span>
+        - a sustainable agroforestry organization dedicated
+        to restoring the Amazon by helping rainforest communities to plant native trees and improve
+        their livelihoods.</p>
+      <span className={s.bottomText}>We are all connected</span>
     </div>
   )
 }
